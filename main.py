61b4aabf9eb16db1bb8020ab84b9c9ee7d3a2628
Davids Berzins <116670695+cfunkz@users.noreply.github.com>
2023-11-07 15:25:51 +0000

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 import wavelink
 from wavelink.ext import spotify
-from config import dtoken, freeURL, freePASS,S spotifyUSER, spotifyPASS
+from config import dtoken, freeURL, freePASS,S spotifyUSER, spotifySECRET
 
 intents = discord.Intents.all()
 
