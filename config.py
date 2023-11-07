e2463ef7b6985909eb122f42a7e41230bf2249cc
Davids Berzins <116670695+cfunkz@users.noreply.github.com>
2023-11-07 11:29:05 +0000

    Update config.py

--- config.py
+++ config.py
@@ -3,5 +3,3 @@ import os
 dtoken = os.environ['token']
 freeURL = os.environ['freeURL']
 freePASS = os.environ['freePASS']
-spotifyUSER = os.environ['spotifyUSER']
-spotifySECRET = os.environ['spotifySECRET']
