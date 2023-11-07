18f26c459ec55ffec33e26a7d7fde9d5bb034518
Davids Berzins <116670695+cfunkz@users.noreply.github.com>
2023-11-07 11:16:52 +0000

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ intents = discord.Intents.all()
 
 class Bot(commands.Bot):
     def __init__(self):
-        super().__init__(command_prefix=commands.when_mentioned_or('420 '), intents=intents, activity = discord.Game(name="/help"), help_command=None)
+        super().__init__(command_prefix=commands.when_mentioned_or('!'), intents=intents, activity = discord.Game(name="/help OR !help"), help_command=None)
 
     async def on_ready(self):
         print(f'Logged in {self.user} | {self.user.id}')
